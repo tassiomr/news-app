d6e3e9bc20b82d3438ff3ac9e2b6ee22b96be43f
Tassio Rocha <=>
2020-05-22 20:54:11 -0300

    feat: add theme provider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import theme from './src/theme';
 import Routes from './src/routes';
 
 const App: React.FC = () => {
+  const themeColor = useColorScheme();
   return (
-    <ThemeProvider theme={theme.darkTheme}>
+    <ThemeProvider theme={theme[themeColor || 'light']}>
       <Routes />
     </ThemeProvider>
   );
