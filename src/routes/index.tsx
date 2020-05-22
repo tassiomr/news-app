d141ee69c3496b0e10563123850a148747470a8f
Tassio Rocha <=>
2020-05-22 20:52:18 -0300

    feat: add icons on tab navigator

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeRoutes from './home.stack';
 import FavoritesRoutes from './favorites.stack';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {ThemeContext} from 'styled-components';
+
+MaterialIcons.loadFont();
 
 const TabNavigator = createBottomTabNavigator();
 
 const TabBar: React.FC = () => {
+  const theme = useContext(ThemeContext);
   return (
-    <TabNavigator.Navigator>
-      <TabNavigator.Screen component={HomeRoutes} name="App" />
+    <TabNavigator.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else {
+            iconName = 'favorite';
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: theme.activeIcon,
+        inactiveTintColor: theme.inactiveIcon,
+        style: {
+          backgroundColor: `${theme.backgroundColor}99`,
+        },
+      }}>
+      <TabNavigator.Screen component={HomeRoutes} name="Home" />
       <TabNavigator.Screen component={FavoritesRoutes} name="Favorites" />
     </TabNavigator.Navigator>
   );
