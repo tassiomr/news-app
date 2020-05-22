71ede653c9750bfa33946e5516a3344c11053d43
Tassio Rocha <=>
2020-05-22 19:22:27 -0300

    styles: init configuration of styles

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+import Palette from './pallete';
+
+type Theme = {
+  backgroundColor: Palette;
+  titleColor: Palette;
+  linkColor: Palette;
+  paragraphColor: Palette;
+};
+
+const lightTheme: Theme = {
+  backgroundColor: Palette.cultured,
+  titleColor: Palette.liverOrgan,
+  linkColor: Palette.midnightGreen,
+  paragraphColor: Palette.pineGreen,
+};
+
+const darkTheme: Theme = {
+  backgroundColor: Palette.cultured,
+  titleColor: Palette.liverOrgan,
+  linkColor: Palette.midnightGreen,
+  paragraphColor: Palette.pineGreen,
+};
+
+export default {darkTheme, lightTheme};
