7a78fe387730cb041306de4f3747e2d35335001d
Tassio Rocha <=>
2020-05-23 10:54:48 -0300

    styles: update header title styles

--- src/components/text/styles.ts
+++ src/components/text/styles.ts
@@ -3,33 +3,46 @@ import {normalize} from '../../../src/configs/helpers';
 import {Platform} from 'react-native';
 
 const size = {
-  ExtraLarge: 32,
+  ExtraLarge: 48,
   large: 24,
   small: 18,
   link: 14,
 };
 
 export const HeaderTitleComponent = styled.Text`
+  font-family: SairaExtraCondensed-Medium;
   font-size: ${normalize(size.ExtraLarge)}px;
   width: 100%;
   text-align: center;
   color: ${(props) => props.theme.titleColor};
+  background-color: ${(props) => props.theme.backgroundColor};
   ${Platform.select({
     ios: {
-      marginTop: 40,
+      shadowColor: '#000',
+      shadowOffset: '0px 5px',
+      shadowOpacity: '0.1px',
+      shadowRadius: '2px',
     },
-  })}
+  })};
 `;
 export const TitleComponent = styled.Text`
   font-size: ${normalize(size.large)}px;
   color: ${(props) => props.theme.titleColor};
+  font-family: SairaExtraCondensed-Medium;
+  padding-left: 8px;
+  padding-right: 8px;
 `;
 export const ParagraphComponent = styled.Text`
   font-size: ${normalize(size.small)}px;
   color: ${(props) => props.theme.paragraphColor};
+  font-family: SairaExtraCondensed-Medium;
+  line-height: 23px;
+  padding-left: 8px;
+  padding-right: 8px;
 `;
 export const LinkComponent = styled.Text`
   font-size: ${normalize(size.link)}px;
   color: ${(props) => props.theme.linkColor};
   text-decoration-line: underline;
+  font-family: SairaExtraCondensed-Medium;
 `;
