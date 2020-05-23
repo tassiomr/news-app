0172d4201be05a20b0c06d4b665355ecc5b57804
Tassio Rocha <=>
2020-05-23 12:42:55 -0300

    styles: add margin top on iPhones

--- src/components/screen/styles.ts
+++ src/components/screen/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
+import {Platform} from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${(props) => props.theme.backgroundColor};
+  ${Platform.select({
+    ios: {
+      marginTop: 20,
+    },
+  })}
 `;
