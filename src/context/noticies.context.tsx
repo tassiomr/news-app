659ff6111ba53799abaf6784f835ed1191f0d504
Tassio Rocha <=>
2020-05-24 10:04:22 -0300

    feat: update notice context to change notice type

--- src/context/noticies.context.tsx
+++ src/context/noticies.context.tsx
@@ -1,29 +1,54 @@
 import React, {createContext, Children, useState, useContext} from 'react';
 import {INotice} from '../typescript/interfaces';
+import {NoticeType} from '../typescript/enums';
 import noticesService from '../../src/services/notices.service';
 
+type notices = {
+  type: NoticeType;
+  data: [INotice?];
+};
 interface NotificeContext {
   isLoading: boolean;
   getNoticies: Function;
-  notices: [INotice?];
+  notices: notices;
+  changePage: Function;
 }
 
 const NoticeContext = createContext<NotificeContext>({} as NotificeContext);
 
 export const NoticeProvider: React.FC = ({children}) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [notices, setNotices] = useState<[INotice?]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [notices, setNotices] = useState<{type: NoticeType; data: [INotice?]}>({
+    type: NoticeType.science,
+    data: [],
+  });
+
+  const [technology, setTechnology] = useState<[INotice?]>([]);
+  const [science, setScience] = useState<[INotice?]>([]);
 
   async function getNoticies() {
-    if (!notices.length) {
+    if (!notices.data.length) {
       noticesService.getScienceNotices().then((e: any) => {
-        setNotices(e);
+        setScience(e);
+        setNotices({...notices, data: e});
+        noticesService.getTechnologyNotices().then((e: any) => {
+          setTechnology(e);
+        });
       });
     }
   }
 
+  function changePage(noticeType: NoticeType) {
+    if (noticeType === NoticeType.science) {
+      setNotices({type: noticeType, data: science});
+    } else {
+      setNotices({type: noticeType, data: technology});
+    }
+  }
+
   return (
-    <NoticeContext.Provider value={{isLoading, notices, getNoticies}}>
+    <NoticeContext.Provider
+      value={{isLoading, notices, getNoticies, changePage}}>
       {children}
     </NoticeContext.Provider>
   );
