df352e89138e39e7af3c100975e3aa1fdc783752
Tassio Rocha <=>
2020-05-24 10:49:36 -0300

    fix: add correct type definition

--- src/components/flastlist/index.tsx
+++ src/components/flastlist/index.tsx
@@ -3,12 +3,12 @@ import {INotice} from '../../typescript/interfaces';
 import {FlatList} from 'react-native';
 import {Card} from '../card';
 
-export const NoticeFlatList: React.FC<{data: INotice}> = ({data}) => {
+export const NoticeFlatList: React.FC<{data: [INotice?]}> = ({data}) => {
   return (
     <FlatList
-      style={{padding: 16}}
+      style={{padding: 16, marginTop: 40}}
       data={data}
-      renderItem={({item}) => <Card notice={item} />}
+      renderItem={({item}) => <Card notice={item!} />}
     />
   );
 };
