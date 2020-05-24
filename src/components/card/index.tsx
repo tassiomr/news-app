5001f01c8b4fb3a6f0d2563f866a89d4f915bf6f
Tassio Rocha <=>
2020-05-24 11:31:10 -0300

    feat: add loader image

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {CardContainer, Image} from './styles';
 import {Text} from '../text';
 import {INotice} from 'src/typescript/interfaces';
+import {Loader} from '../loader';
 
-export const Card: React.FC<{notice: INotice}> = ({notice}) => (
-  <CardContainer>
-    <Image source={{uri: notice.picture}} />
-    <Text.Title text={notice.title} />
-    <Text.Paragraph text={notice.resume} />
-  </CardContainer>
-);
+export const Card: React.FC<{notice: INotice}> = ({notice}) => {
+  const [loadImage, setLoadImage] = useState(false);
+
+  return (
+    <CardContainer>
+      <Image
+        source={{uri: notice.picture, cache: 'force-cache'}}
+        onLoadStart={() => {
+          setLoadImage(true);
+        }}
+        onLoadEnd={() => {
+          setLoadImage(false);
+        }}>
+        {loadImage && <Loader />}
+      </Image>
+      <Text.Title text={notice.title} />
+      <Text.Paragraph text={notice.resume} />
+    </CardContainer>
+  );
+};
