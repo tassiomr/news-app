a45f44b5f72b202b844b8b6eb22ba9e303c9aaa6
Tassio Rocha <=>
2020-05-25 14:20:12 -0300

    styles: adjusts on card ui interface

--- src/components/card/styles.ts
+++ src/components/card/styles.ts
@@ -1,32 +1,38 @@
 import styled from 'styled-components/native';
-import {Platform, ScrollView} from 'react-native';
+import {Platform} from 'react-native';
 import {normalize} from '../../../src/configs/helpers';
 
 export const Image = styled.ImageBackground.attrs((props) => ({
   imageStyle: {
-    borderTopRightRadius: 4,
-    borderTopLeftRadius: 4,
+    borderRadius: 4,
   },
 }))`
-  height: 200px;
-  width: 100%;
+  height: ${normalize(50)}px;
+  width: ${normalize(50)}px;
   justify-content: center;
   align-items: center;
 `;
 
 export const CardContainer = styled.TouchableOpacity`
-  min-height: ${normalize(200)}px;
+  min-height: ${normalize(50)}px;
   width: 100%;
   margin-bottom: 16px;
-  padding-bottom: 8px;
   background-color: ${(props) => props.theme.backgroundColor};
   border-radius: 4px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   ${Platform.select({
     ios: {
       shadowColor: '#000',
-      shadowOpacity: '0.5',
+      shadowOpacity: '0.2',
       shadowRadius: 5,
-      shadowOffset: '1px 1px',
+      shadowOffset: '0px 0px',
     },
   })};
+`;
+
+export const TextWrapper = styled.View`
+  flex: 1;
+  justify-content: flex-start;
 `;
