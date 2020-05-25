beb08a5f42039a3591e36c7648054eee4b1f1cd6
Tassio Rocha <=>
2020-05-25 14:20:48 -0300

    feat: add open modal function on card

--- src/components/flastlist/index.tsx
+++ src/components/flastlist/index.tsx
@@ -6,10 +6,11 @@ import {Card} from '../card';
 export const NoticeFlatList: React.FC<{data: [INotice?]}> = ({data}) => {
   return (
     <FlatList
-      keyExtractor={(item) => item?.id}
-      style={{padding: 8, marginTop: 40}}
+      contentContainerStyle={{paddingTop: 16, paddingBottom: 80}}
+      keyExtractor={(item) => item?.id!}
+      style={{padding: 16}}
       data={data}
-      renderItem={({item}) => <Card notice={item!} />}
+      renderItem={({item}) => <Card openModal={() => {}} notice={item!} />}
     />
   );
 };
