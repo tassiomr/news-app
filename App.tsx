f7871649bfdc96c6de04e2ce754eec0dfd300c78
Tassio Rocha <=>
2020-05-25 16:38:43 -0300

    improviment: add notice provider

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import {useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import theme from './src/theme';
 import Routes from './src/routes';
+import {NoticeProvider} from './src/context/noticies.context';
 
 const App: React.FC = () => {
   const themeColor = useColorScheme();
   return (
     <ThemeProvider theme={theme[themeColor || 'light']}>
-      <Routes />
+      <NoticeProvider>
+        <Routes />
+      </NoticeProvider>
     </ThemeProvider>
   );
 };
